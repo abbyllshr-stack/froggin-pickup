e908a55e01ccb30f90764aad78a33b20c4365745
abbyllshr-stack <abby.ll.sh.r@gmail.com>
2026-07-09 22:02:21 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,7 @@ const CONFIG = {
 
     API_URL: "https://script.google.com/macros/s/AKfycbxnfKzj1Fdd31lNaRgEveoz_Fk0z28tB61LhTRry6pFdZTfSZb3HXtzV0YF6opYdjoC/exec",
 
-    APP_NAME: "🧪 Froggin Pickup Colima"
-
-    REFRESH_TIME: 2000,
+        REFRESH_TIME: 2000,
 
     RESULT_TIME: 2000,
 
