e45bc2d52638984b849a183cf54c8f63ccfea2f1
abbyllshr-stack <abby.ll.sh.r@gmail.com>
2026-07-16 19:15:59 -0600

    Fix formatting of CONFIG object in config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
 
     API_URL: "https://script.google.com/macros/s/AKfycbxnfKzj1Fdd31lNaRgEveoz_Fk0z28tB61LhTRry6pFdZTfSZb3HXtzV0YF6opYdjoC/exec",
 
-        REFRESH_TIME: 2000,
+    REFRESH_TIME: 2000,
 
     RESULT_TIME: 2000,
 
